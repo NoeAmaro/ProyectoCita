be3d5ba187cc9b1ccc3259f426e43a5805414837
NoeAmaro <161240758+NoeAmaro@users.noreply.github.com>
2024-05-29 19:59:15 -0600

    script.js

--- script.js
+++ script.js
@@ -1,20 +1,27 @@
 document.getElementById('appointment-form').addEventListener('submit', function(event) {
-    event.preventDefault(); // Evita que el formulario se envíe de la forma tradicional
-  
-    const name = document.getElementById('name').value;
-    const email = document.getElementById('email').value;
-    const phone = document.getElementById('phone').value;
-    const service = document.getElementById('service').value;
-    const date = document.getElementById('date').value;
-    const time = document.getElementById('time').value;
-    const message = document.getElementById('message').value;
-  
-    // Aquí puedes agregar la lógica para enviar los datos del formulario a un servidor
-  
-    document.getElementById('appointment-form').reset();
-    document.getElementById('confirmation').classList.remove('hidden');
-    setTimeout(() => {
-      document.getElementById('confirmation').classList.add('hidden');
-    }, 3000);
-  });
-  
+    event.preventDefault();
+
+    const formData = new FormData(event.target);
+    const data = Object.fromEntries(formData.entries());
+
+    fetch('https://script.google.com/macros/s/AKfycbzihmkYe5Le5IJcSGARxL9FKAOavJ0kkACt4pxlwkXLD7jOefx5KAWkaPzK8ijo4g4s8Q/exec', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.status === 'success') {
+            document.getElementById('confirmation').classList.remove('hidden');
+            event.target.reset();
+        } else {
+            alert('Hubo un error al agendar su cita. Inténtelo de nuevo.');
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        alert('Hubo un error al agendar su cita. Inténtelo de nuevo.');
+    });
+});
